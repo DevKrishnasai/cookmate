d71d4ecfb1fea55c29202aa2351c4030b470e426
devkrishna <ambatikrishnasai1301@gmail.com>
2024-08-04 23:54:10 +0530

    added few SEO optimizations

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -3,6 +3,13 @@ import SideBarWraper from "@/components/SideBarWraper";
 import { auth } from "@clerk/nextjs/server";
 import { getUserElseCreate } from "@/actions/user";
 
+//add metadata
+export const metadata = {
+  title: "CookMate | Your Personal Recipe Hub",
+  description:
+    "Your personal recipe hub for creating, sharing, and discovering amazing recipes",
+};
+
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const { userId } = auth();
   if (userId) {
